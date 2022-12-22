604d4573ec398398f808c4804a54a36cee78865a
ashokkujur@amazingworkz.com <ashokkujur@gmail.com>
2022-12-22 12:14:19 +0530

    font family implemeted

--- lib/styles/text_style.dart
+++ lib/styles/text_style.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movie_flutter_test_app/assets/font_family.dart';
 import 'package:movie_flutter_test_app/styles/app_colors.dart';
 
 class AppTextStyle {
-  static TextStyle size24ColorWhite =
-      const TextStyle(fontSize: 24.0, color: Colors.white);
-  static TextStyle size24ColorBlack =
-      const TextStyle(fontSize: 24.0, color: Colors.black);
-  static TextStyle size20ColorWhite =
-      const TextStyle(fontSize: 20.0, color: Colors.blue);
-  static TextStyle size18ColorBlack =
-      const TextStyle(fontSize: 18.0, color: Colors.black);
-  static TextStyle size14Colorprimary =
-      TextStyle(fontSize: 14.0, color: Color(AppColor.primaryColor));
-  static TextStyle size18Colorblack =
-      TextStyle(fontSize: 18.0, color: Colors.black);
+  static TextStyle size24ColorWhite = const TextStyle(
+      fontSize: 24.0, color: Colors.white, fontFamily: AppFonts.poppinsBlack);
+  static TextStyle size24ColorBlack = const TextStyle(
+      fontSize: 24.0, color: Colors.black, fontFamily: AppFonts.poppinsBlack);
+  static TextStyle size20ColorWhite = const TextStyle(
+      fontSize: 20.0, color: Colors.blue, fontFamily: AppFonts.poppinsBlack);
+  static TextStyle size18ColorBlack = const TextStyle(
+      fontSize: 18.0, color: Colors.black, fontFamily: AppFonts.poppinsBlack);
+  static TextStyle size14Colorprimary = const TextStyle(
+      fontSize: 14.0,
+      color: Color(AppColor.primaryColor),
+      fontFamily: AppFonts.poppinsBlack);
+  static TextStyle size18Colorblack = const TextStyle(
+      fontSize: 18.0, color: Colors.black, fontFamily: AppFonts.poppinsBlack);
 }
